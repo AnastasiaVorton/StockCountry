711795528533b745725f6d8e0b2abd3f9a092161
anastasiareprintseva <a.reprinceva.kazanexpress@gmail.com>
2023-01-12 20:01:49 +0500

    feat(text field): add label, props

--- src/components/TextField.vue
+++ src/components/TextField.vue
@@ -1,8 +1,37 @@
 <template>
-  <input class="text-field" />
+  <div class="text-field__container">
+    <label :for="id">{{ label }}</label>
+    <input
+      :id="id"
+      class="text-field"
+      :value="modelValue"
+      :placeholder="placeholder"
+      :maxlength="maxlength"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+  </div>
 </template>
 
+<script lang="ts">
+export default {
+  name: "TextField",
+  props: {
+    modelValue: String,
+    emits: ["update:modelValue"],
+    label: String,
+    id: String,
+    placeholder: String,
+    maxlength: Number,
+  },
+};
+</script>
+
 <style lang="scss">
+.text-field__container {
+  display: flex;
+  flex-direction: column;
+}
+
 .text-field {
   padding: 12px 0;
   border: none;
