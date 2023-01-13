3d9b83c45b4da2f6927d7c46591178dbfe3dc826
anastasiareprintseva <a.reprinceva.kazanexpress@gmail.com>
2023-01-13 21:18:34 +0500

    feat(stock): display watch list

---
+++ src/components/Stock.vue
@@ -0,0 +1,27 @@
+<template>
+  <Card>
+    <div>ISIN: {{ isin }}</div>
+    <div>Price: {{ price }}</div>
+    <Button @buttonClick="$emit('unsubscribe', isin)">Unsubscribe</Button>
+  </Card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Button from "./Button.vue";
+import Card from "./Card.vue";
+
+export default defineComponent({
+  name: "Stock",
+  components: {
+    Button,
+    Card,
+  },
+  props: {
+    isin: String,
+    price: Number,
+  },
+});
+</script>
+
+<style scoped></style>
